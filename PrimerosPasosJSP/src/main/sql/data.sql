a2d206e6fa1897a9da61361558046eb457560c25
Cristian Cardozo <cristian.cardozo@ticketmaster.com>
2023-01-31 14:29:13 -0500

    fix paths to run over mac, modify pass for databases and append sql scripts.

---
+++ PrimerosPasosJSP/src/main/sql/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO empleados (nombre, apellido, puesto, salario) 
+	VALUES 
+		('Camilo', 'Cardozo', 'Desarrollador', 60000.00),
+		('Pedro', 'Heredia', 'Vendedor', 6000.00),
+		('Simon', 'Bolivar', 'Auditora', 30000.00),
+		('Francisco', 'Snatander', 'Sastre', 600.00),
+		('Policarpa', 'Salabarrieta', 'Caudillo', 60.00);
---
+++ PrimerosPasosJSP/src/main/sql/tables.sql
@@ -0,0 +1,5 @@
+CREATE TABLE empleados (
+	nombre varchar(20),
+	apellido varchar(20),
+	puesto varchar(40),
+	salario double(10, 2));
